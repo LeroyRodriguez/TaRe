f27cd395aeeec707532cab7de2c111bacd37c5fc
Megaport <Megaport@DESKTOP-98B3OOV>
2020-09-19 11:23:07 +0200

    erste Änderung

---
+++ TaRe/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaRe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gib den ersten Summanden ein ");
+            string erstersummand = Console.ReadLine();
+            Console.Write("Gib den zweiten Summanden ein ");
+            string zweitersummand = Console.ReadLine();
+            Console.ReadLine();
+
+            int ersterSummandalsZahl = Convert.ToInt32(erstersummand);
+            int zweiterSummandalsZahl = Convert.ToInt32(zweitersummand);
+
+            int summe = ersterSummandalsZahl + zweiterSummandalsZahl;
+
+            Console.WriteLine("Die Summe ist: {0}", summe);
+            Console.ReadLine();
+        }
+    }
+}
