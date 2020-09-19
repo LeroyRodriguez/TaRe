cc56f7b4f79892921c93e0388cdf7af19ac9082c
Megaport <Megaport@DESKTOP-98B3OOV>
2020-09-19 15:25:08 +0200

    Ganzzahlen

--- TaRe/Program.cs
+++ TaRe/Program.cs
@@ -15,12 +15,13 @@ namespace TaRe
             string erstersummand = Console.ReadLine();
             Console.Write("Gib den zweiten Summanden ein ");
             string zweitersummand = Console.ReadLine();
-            
+            Console.ReadLine();
+
             //Wandel Text in Numerische Werte um
-            float ersterSummandalsZahl = Convert.ToSingle(erstersummand);
-            float zweiterSummandalsZahl = Convert.ToSingle(zweitersummand);
-            // Berechnung ausführen
-            float summe = ersterSummandalsZahl + zweiterSummandalsZahl;
+            int ersterSummandalsZahl = Convert.ToInt32(erstersummand);
+            int zweiterSummandalsZahl = Convert.ToInt32(zweitersummand);
+        
+            int summe = ersterSummandalsZahl + zweiterSummandalsZahl;
 
             Console.WriteLine("Die Summe ist: {0}", summe);
             Console.ReadLine();
