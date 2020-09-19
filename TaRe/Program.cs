065c562753727c023d539948c51b900ffbef9bf6
Megaport <Megaport@DESKTOP-98B3OOV>
2020-09-19 15:23:16 +0200

    Gleitzahlen eingegeben

--- TaRe/Program.cs
+++ TaRe/Program.cs
@@ -10,16 +10,17 @@ namespace TaRe
     {
         static void Main(string[] args)
         {
+            // Addieren von zwei Zahlen
             Console.WriteLine("Gib den ersten Summanden ein ");
             string erstersummand = Console.ReadLine();
             Console.Write("Gib den zweiten Summanden ein ");
             string zweitersummand = Console.ReadLine();
-            Console.ReadLine();
-
-            int ersterSummandalsZahl = Convert.ToInt32(erstersummand);
-            int zweiterSummandalsZahl = Convert.ToInt32(zweitersummand);
-
-            int summe = ersterSummandalsZahl + zweiterSummandalsZahl;
+            
+            //Wandel Text in Numerische Werte um
+            float ersterSummandalsZahl = Convert.ToSingle(erstersummand);
+            float zweiterSummandalsZahl = Convert.ToSingle(zweitersummand);
+            // Berechnung ausführen
+            float summe = ersterSummandalsZahl + zweiterSummandalsZahl;
 
             Console.WriteLine("Die Summe ist: {0}", summe);
             Console.ReadLine();
